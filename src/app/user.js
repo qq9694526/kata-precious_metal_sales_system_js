33a0480e63b9fee4ec5a61c99c9dfff7835d69e0
赵海鹏 <9694526@qq.com>
2019-07-02 17:37:40 +0800

    user类新增getUserLevel方法,用于根据积分获取客户等级

--- src/app/user.js
+++ src/app/user.js
@@ -9,12 +9,37 @@ export default class User {
     }
 
     getUser() {
+        const point=19720;
         return {
             memberNo: '6236609999',
             memberName: '马丁',
             memberPoIncreased: 2,
-            memberPoints: 19720,
-            newMemberType: '金卡'
+            memberPoints: point,
+            newMemberType: this.getUserLevel(point).name
         };
+    }
+
+    getUserLevel(point) {
+        const data = [{
+            name: '普卡',
+            scope: [0, 10000],
+            multiple: 1
+        }, {
+            name: '金卡',
+            scope: [10000, 50000],
+            multiple: 1.5
+        }, {
+            name: '白金卡',
+            scope: [50000, 100000],
+            multiple: 1.8
+        }, {
+            name: '钻石卡',
+            scope: [100000, Infinity],
+            multiple: 2
+        }];
+        const result=data.filter(item=>{
+            return  point >=item.scope[0]&&point <item.scope[1];
+        });
+        return result[0]||{};
     }
 }
--- test/order-app.spec.js
+++ test/order-app.spec.js
@@ -3,6 +3,7 @@ import { basename, sep } from 'path';
 import OrderApp from '../src/order-app';
 import { readFile } from '../src/output/utils';
 
+import User from '../src/app/user';
 describe('OrderApp', () => {
   const resourcesDir = `${__dirname}${sep}resources${sep}`;
   const parameters = [
@@ -28,6 +29,30 @@ describe('OrderApp', () => {
       memberPoIncreased: 2,
       memberPoints: 19720,
       newMemberType: '金卡'
+    });
+  });
+  it('当调用 User.getUserLevel() 方法，入参为1922，期望得到用户等级为普卡', async () => {
+    const actualRepresentation = (new User()).getUserLevel(1922);
+    assert.deepEqual(actualRepresentation, {
+      name: '普卡',
+      scope: [0, 10000],
+      multiple: 1
+    });
+  });
+  it('当调用 User.getUserLevel() 方法，入参为10000，期望得到用户等级为金卡', async () => {
+    const actualRepresentation = (new User()).getUserLevel(10000);
+    assert.deepEqual(actualRepresentation, {
+      name: '金卡',
+      scope: [10000, 50000],
+      multiple: 1.5
+  });
+  });
+  it('当调用 User.getUserLevel() 方法，入参为9999999，期望得到用户等级为钻石卡', async () => {
+    const actualRepresentation = (new User()).getUserLevel(9999999);
+    assert.deepEqual(actualRepresentation, {
+      name: '钻石卡',
+      scope: [100000, Infinity],
+      multiple: 2
     });
   });
 
